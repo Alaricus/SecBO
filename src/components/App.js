17aebc1a5cc2430a551ca8ac65f587ab9384450a
Alaric von Teplitz <alaric2000@gmail.com>
2017-10-07 13:20:07 -0600

    Added image drag/upload and a rudimentary preview.

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,68 @@ import '../styles.css';
 
 export default class App extends Component {
   state = {
-    text: "",
-    binary: "",
-  }
+    text: '',
+    binary: '',
+    b2t: '',
+    image: '',
+  };
 
   handleChange(e) {
-    let binary = '';
-    e.target.value.split('').forEach((char) => {
-      binary += char.charCodeAt(0).toString(2) + ' ';
+    this.setState({ text: e.target.value });
+  }
+
+  textToBinary(txt) {
+    let bin = '';
+    txt.split('').forEach((c) => {
+      bin += c.charCodeAt(0).toString(2) + ' ';
     });
-    this.setState({ 
-      text: e.target.value,
-      binary: binary,
+    return bin;
+  }
+
+  binaryToText(bin) {
+    let txt = '';
+    bin.split(' ').forEach((b) => {
+      txt += String.fromCharCode(parseInt(b, 2));
+    });
+    return txt;
+  }
+
+  uploadImage(e) {
+    const self = this;
+    e.preventDefault();
+    const fr = new FileReader();
+    const img = new Image();
+    fr.onload = function(event) {
+        try {
+            img.onload = () => {
+              self.setState({image: img});
+            }
+            img.src = event.target.result;
+
+        } catch (err) {
+            // uploadFailure(imgUploadArea);
+            alert('File failed to load.')
+        }
+    }
+    // Only accept png images and only use the first file if several are dragged.
+    const file = e.dataTransfer.files[0];
+    if (file.type === "image/png") {
+        fr.readAsDataURL(file);
+    } else {
+        // uploadFailure(imgUploadArea);
+        alert('Invalid format. Use a PNG image.');
+    }
+  }
+
+  processTo() {
+    this.setState({
+      binary: this.textToBinary(this.state.text)
+    });
+  }
+
+  processFrom() {
+    this.setState({
+      b2t: this.binaryToText(this.state.binary)
     });
   }
 
@@ -22,12 +72,16 @@ export default class App extends Component {
     return (
       <div className="App">
         <h2>SecBO</h2>
-        <div className="ImageLoader">
-          drag image here or click to load
+        <div className="ImageUploader"
+          onDrop={(e) => { this.uploadImage(e); }}
+          onDragEnter={(e) => {e.preventDefault();}}
+          onDragOver={(e) => {e.preventDefault();}}
+        >
+          <br />drag an image file here to import.
+          <div className="ImagePreview" style={{backgroundImage: `url(${this.state.image.src})`}}></div>
         </div>
         <textarea rows="15" cols="100" value={ this.state.text } onChange={(e) => { this.handleChange(e); }}>
         </textarea>
-        <p>{ this.state.binary }</p>
       </div>
     );
   }
