25c3c0ec919ca5fb98abcdd9428732fdedb602a0
Alaric von Teplitz <alaric2000@gmail.com>
2017-10-07 21:26:27 -0600

    Everything mostly works. Just need to refactor, etc. now.

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,24 @@ export default class App extends Component {
   state = {
     text: '',
     binary: '',
-    b2t: '',
     image: '',
   };
 
   handleChange(e) {
-    this.setState({ text: e.target.value });
+    this.setState({ text: e.target.value, binary: this.textToBinary(e.target.value) });
+  }
+
+  refesh() {
+    this.setState({ 
+      text: '',
+      binary: '',
+      image: '',
+    });
   }
 
   textToBinary(txt) {
     let bin = '';
+    txt = `[secbo]${txt}[/secbo]`;
     txt.split('').forEach((c) => {
       bin += c.charCodeAt(0).toString(2) + ' ';
     });
@@ -34,16 +42,19 @@ export default class App extends Component {
     e.preventDefault();
     const fr = new FileReader();
     const img = new Image();
-    fr.onload = function(event) {
+    fr.onload = async (event) => {
         try {
-            img.onload = () => {
-              self.setState({image: img});
-            }
-            img.src = event.target.result;
-
+          img.onload = async () => {
+            await self.setState({image: img});
+            // For some reason using self.updateCanvas() as callback of setState
+            // didn't work. So using async/await instead. Weird.
+            self.updateCanvas();
+            self.readAlpha();
+            self.getDownload();
+          }
+          img.src = event.target.result;
         } catch (err) {
-            // uploadFailure(imgUploadArea);
-            alert('File failed to load.')
+          alert('File failed to load.')
         }
     }
     // Only accept png images and only use the first file if several are dragged.
@@ -51,21 +62,71 @@ export default class App extends Component {
     if (file.type === "image/png") {
         fr.readAsDataURL(file);
     } else {
-        // uploadFailure(imgUploadArea);
         alert('Invalid format. Use a PNG image.');
     }
   }
 
-  processTo() {
-    this.setState({
-      binary: this.textToBinary(this.state.text)
-    });
+  updateCanvas(img) {
+    const ctx = this.refs.canvas.getContext('2d');
+    ctx.canvas.width = this.state.image.width;
+    ctx.canvas.height = this.state.image.height;
+    ctx.drawImage(this.state.image, 0, 0);
   }
 
-  processFrom() {
-    this.setState({
-      b2t: this.binaryToText(this.state.binary)
+  readAlpha() {
+    const ctx = this.refs.canvas.getContext('2d');
+    const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+    let bin = '';
+    imageData.data.forEach((value, index) => {
+      if (index > 0 && (index + 1) % 4 === 0 ) {
+        if (value === 253) { 
+          bin += ' ';
+        }
+        if (value === 254) { 
+          bin += '1';
+        }
+        if (value === 255) {
+          bin += '0';
+        }
+      }
     });
+    
+    const text = this.binaryToText(bin);
+    if (text.startsWith('[secbo]')
+      && text.includes('[/secbo]')) {
+      this.setState({text: text.substring(7, text.length - 9)});
+    }
+  }
+
+  writeAlpha() {
+    const ctx = this.refs.canvas.getContext('2d');
+    let imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+    if (this.state.binary.length > 0) {
+      this.state.binary.split('').forEach((digit, index) => {
+        // This sets everything to 255, so no need to handle zeroes
+        imageData.data[(index * 4) + 3] = 255;
+
+        if (digit === '1') {
+          // imageData.data[index * 4] = 0;
+          // imageData.data[(index * 4) + 1] = 0;
+          // imageData.data[(index * 4) + 2] = 0;
+          imageData.data[(index * 4) + 3] = 254;
+        }
+        
+        if (digit === ' ') {
+          // imageData.data[index * 4] = 0;
+          // imageData.data[(index * 4) + 1] = 0;
+          // imageData.data[(index * 4) + 2] = 255;
+          imageData.data[(index * 4) + 3] = 253;
+        }
+      });
+      ctx.putImageData(imageData, 0, 0);
+      this.getDownload();
+    }
+  }
+
+  getDownload() {
+    this.setState({dl: this.refs.canvas.toDataURL("image/png")});
   }
 
   render() {
@@ -80,8 +141,19 @@ export default class App extends Component {
           <br />drag an image file here to import.
           <div className="ImagePreview" style={{backgroundImage: `url(${this.state.image.src})`}}></div>
         </div>
-        <textarea rows="15" cols="100" value={ this.state.text } onChange={(e) => { this.handleChange(e); }}>
+        <textarea rows="15" cols="50" value={ this.state.text } onChange={(e) => { this.handleChange(e); }}>
         </textarea>
+        <br />
+        <canvas ref="canvas"></canvas>
+        <br />
+        {
+          this.state.text.length > 0 && this.state.image.src 
+          && <a 
+            href={this.state.dl} 
+            download="test.png" 
+            onClick={() => { this.writeAlpha(); }}>download
+          </a>
+        }
       </div>
     );
   }
